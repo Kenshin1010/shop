190cde25076e740c9885d3cd69bcbd02c3d943d0
anh.do <tuananh9h@gmail.com>
2024-09-21 16:58:40 +0700

    fetchChartData

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,13 @@
 import httpRequest from '../utils/httpRequest';
+
+export const fetchChartData = async (
+  days: string,
+  merchant: string,
+  metric: string
+) => {
+  const response = await fetch(
+    `/get_chart_data?days=${days}&merchant=${merchant}&metric=${metric}`
+  );
+  const data = await response.json();
+  return data;
+};
